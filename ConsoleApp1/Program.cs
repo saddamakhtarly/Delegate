81124cb14956f354666dd4ce30307577ca5aa84e
Saddam Akhtar <saddamakhtar.ly@gmail.com>
2020-04-04 13:50:22 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.LongRunning(CallBack); 
+
+            Console.ReadKey();
+        }
+        static void CallBack(int i)
+        {            
+            Console.WriteLine(i);
+        }
+    }
+    public class MyClass
+    {
+        public delegate void CallBack(int i);
+        public void LongRunning(CallBack obj)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                obj(i);
+            }
+        }
+    }
+    
+    
+}
